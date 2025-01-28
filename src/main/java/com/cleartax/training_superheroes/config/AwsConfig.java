da47db566736009b65f0ef76dcfd66ce2afcaef1
srajal.dwivedi <srajal.dwivedi@clear.in>
2025-01-28 13:42:34 +0530

    updates

---
+++ src/main/java/com/cleartax/training_superheroes/config/AwsConfig.java
@@ -0,0 +1,25 @@
+package com.cleartax.training_superheroes.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+import java.net.URI;
+
+@Configuration
+public class AwsConfig {
+
+    @Bean
+    public S3Client s3Client() {
+        return S3Client.builder()
+                .region(Region.of("us-east-1"))
+                .credentialsProvider(StaticCredentialsProvider.create(
+                        AwsBasicCredentials.create("test", "test")))
+                .endpointOverride(URI.create("http://localhost:4566"))
+                .build();
+    }
+}
---
+++ src/main/java/com/cleartax/training_superheroes/controllers/SuperheroController.java
@@ -0,0 +1,74 @@
+package com.cleartax.training_superheroes.controllers;
+
+
+import com.cleartax.training_superheroes.config.SqsConfig;
+import com.cleartax.training_superheroes.entities.Superhero;
+import com.cleartax.training_superheroes.dto.SuperheroRequestBody;
+import com.cleartax.training_superheroes.services.SuperheroConsumer;
+import com.cleartax.training_superheroes.services.SuperheroService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import software.amazon.awssdk.services.sqs.SqsClient;
+import software.amazon.awssdk.services.sqs.model.ReceiveMessageRequest;
+import software.amazon.awssdk.services.sqs.model.ReceiveMessageResponse;
+import software.amazon.awssdk.services.sqs.model.SendMessageRequest;
+import software.amazon.awssdk.services.sqs.model.SendMessageResponse;
+
+@RestController
+public class SuperheroController {
+
+    private SuperheroService superheroService;
+
+    @Autowired
+    private SqsConfig sqsConfig;
+
+    @Autowired
+    private SqsClient sqsClient;
+
+    @Autowired
+    private SuperheroConsumer superheroConsumer;
+
+    @Autowired
+    public SuperheroController(SuperheroService superheroService, SqsClient sqsClient){
+        this.superheroService = superheroService;
+        this.sqsClient = sqsClient;
+    }
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "username", defaultValue = "World") String username) {
+        sqsClient.sendMessage(SendMessageRequest.builder()
+            .queueUrl(sqsConfig.getQueueUrl())
+            .messageBody("SpiderMan")
+            .build());
+
+
+        return String.format("Hello %s!, %s", username);
+    }
+
+    @GetMapping("/update_superhero_async")
+    public String updateSuperhero(@RequestParam(value = "superHeroName", defaultValue = "ironMan") String superHeroName) {
+        SendMessageResponse result = sqsClient.sendMessage(SendMessageRequest.builder()
+            .queueUrl(sqsConfig.getQueueUrl())
+            .messageBody(superHeroName)
+            .build());
+
+        return String.format("Message sent to queue with message id %s and superHero %s", result.messageId(), superHeroName);
+    }
+
+
+    @GetMapping("/get_message_from_queue")
+    public String getMessage() {
+       return superheroConsumer.consumeSuperhero();
+    }
+
+//    @GetMapping("/superhero")
+//    public Superhero getSuperhero(@RequestParam(value = "name", defaultValue = "Batman") String name,
+//                                  @RequestParam(value = "universe", defaultValue = "DC") String universe){
+//        return superheroService.getSuperhero(name, universe);
+//    }
+
+    @PostMapping("/superhero")
+    public Superhero persistSuperhero(@RequestBody SuperheroRequestBody superheroRequestBody){
+        return superheroService.persistSuperhero(superheroRequestBody, superheroRequestBody.getUniverse());
+    }
+}
